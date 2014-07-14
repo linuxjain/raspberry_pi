f68f3b100ed2c463456a71d5f8a2d54aa2451b05
saurabh jain <saurabh4768jain@gmail.com>
2014-07-14 18:31:41 +0530

    Update main.s

--- project_1/source/main.s
+++ project_1/source/main.s
@@ -3,23 +3,23 @@
 	Ok LED is connected to the 16th GPIO pin of the raspberry pi board.
 */
 
-	.section .init  // initial code of the kernel image 
+	.section .init  /* initial code of the kernel image */ 
 	
-	.global _start 	// first block of code where the execution will jump.
+	.global _start 	/* first block of code where the execution will jump.*/
 	
 	_start :
 	
 	#####################################
 	
-			ldr r0 , =0x20200000 ;  // GPIO pin controller register address
+			ldr r0 , =0x20200000 ;  /* GPIO pin controller register address */
 			
 	#####################################		
 			
 			
 	#####################################
 			
-			mov r1 , #1 ;			// This block of code set the 16th GPIO pin to work as a output pin.
-			lsl r1 , #18 ;			// For more information please look at the description file.
+			mov r1 , #1 ;			/* This block of code set the 16th GPIO pin to work as a output pin. */
+			lsl r1 , #18 ;			/* For more information please look at the description file. */
 			str r1 , [ r0 , #4] ;
 			
 	#####################################
@@ -27,8 +27,8 @@
 	
 	#####################################		
 			
-			mov r1 , #1 ;			// This block of code set the 16th GPIO pin as Active Low i.e. whenever the signal will go low (in this 
-									//	project all the time ) , OK LED will ligt. For more information please look at the description file.
+			mov r1 , #1 ;			/* This block of code set the 16th GPIO pin as Active Low i.e. whenever the signal will go low (in this 
+							project all the time ) , OK LED will ligt. For more information please look at the description file. */
 			lsl r1 , #16 ;
 			str r1 , [r0 , #40] ;
 			
@@ -36,7 +36,7 @@
 			
 	#####################################		
 			
-	loop$ :							// This block of code will keep busy CPU to execute branch instruction for infinite time.	
+	loop$ :							/* This block of code will keep busy CPU to execute branch instruction for infinite time. */	
 			b loop$ 		
 			
 	#####################################
